0af436cb45bf91c0ec2a8bf3304c888cf78e5535
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-26 22:28:40 +0700

    Add header

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ import './style.css';
 
 // element.appendChild(myIcon);
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+   
+// }
   
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-}
-  
-document.body.appendChild(component());
+// document.body.appendChild(component());
