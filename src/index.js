a20f84c4330fef443bb593a9c3864c3bf99ec71f
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-28 10:47:13 +0700

    Add function to navigate to notes display

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const display = document.querySelector('.main');
 
 const toDoFolders = document.querySelectorAll('.todo-folder');
 
+// navigate to home/today/week
 toDoFolders.forEach(folder => {
     folder.addEventListener("click", e => domManipulator.changeFolder(e, todos, display));
 })
+
+// navigate to notes menu
+document.querySelector('#notes-nav').addEventListener('click', () => notesManager.arrangeNotes(notes));
+document.querySelector('#notes-nav').addEventListener('click', (e) => domManipulator.updateActiveNavMain(e));
