a6887ec6598328d151c3fc908c1854b5ea11ef50
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-28 12:01:20 +0700

    Add side bar in create new function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ const toDoFolders = document.querySelectorAll('.todo-folder');
 const openForm = document.querySelector('.new-todo');
 const overlayNew = document.querySelector('.overlay-new');
 const addToDoForm = document.querySelector('.create-new');
+const newToDoLink = document.querySelector('#new-todo-link'); 
+const newProjectLink = document.querySelector('#new-project-link'); 
+const newNoteLink = document.querySelector('#new-note-link'); 
+const newToDoMenu = document.querySelector('#new-todo-menu');
+const newProjectMenu = document.querySelector('#new-project-menu');
+const newNoteMenu = document.querySelector('#new-note-menu');
 
 // navigate to home/today/week
 toDoFolders.forEach(folder => {
@@ -24,4 +30,39 @@ document.querySelector('#notes-nav').addEventListener('click', (e) => domManipul
 openForm.addEventListener('click', () => {
     overlayNew.classList.toggle('overlay-new-invisible');
     addToDoForm.classList.toggle('create-new-open');
+})
+
+// control which form menu is open 
+newToDoLink.addEventListener('click', () =>{
+    // turn off other menus
+    newProjectMenu.style.display = "none";
+    newNoteMenu.style.display = "none";
+    // DISPLAY SELECTED MENU
+    newToDoMenu.style.display = "flex";
+})
+
+newProjectLink.addEventListener('click', () =>{
+    // turn off other menus
+    newToDoMenu.style.display = "none";
+    newNoteMenu.style.display = "none";
+    // DISPLAY SELECTED MENU
+    newProjectMenu.style.display = "flex";
+})
+
+newNoteLink.addEventListener('click', () =>{
+    // turn off other menus
+    newToDoMenu.style.display = "none";
+    newProjectMenu.style.display = "none";
+    // DISPLAY SELECTED MENU
+    newNoteMenu.style.display = "flex";
+})
+
+const createNewOptions = document.querySelectorAll('.create-new__options-items');
+createNewOptions.forEach(option => {
+    option.addEventListener('click', e => {
+        createNewOptions.forEach(option => {
+            option.classList.remove('create-new__options-items-active');
+        });
+        e.target.classList.add('create-new__options-items-active');
+    });
 })
